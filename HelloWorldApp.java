ccf3f32ffa630ff87b1c862d92d99ef059c48d9a
ki4oir <curtis.morris11@student.ctuonline.edu>
2014-11-15 18:37:41 -0500

    Create HelloWorldApp.java
    This is the famous HelloWorldApp.java.  
    
    Oracle.com. (2014). "Hello World!" for the NetBeans IDE. Retrieved from https://docs.oracle.com/javase/tutorial/getStarted/cupojava/netbeans.html.

---
+++ HelloWorldApp.java
@@ -0,0 +1,19 @@
+package helloworldapp;
+
+/**
+* The HelloWorldApp class implements a java application that
+*simply prints "Hello World!" to standard output.
+*/
+
+public class HelloWorldApp {
+
+/**
+* @param args the command line arguments
+*/
+
+  public static void main (String[] args) {
+    System.out.println("Hello World!"); //Display the string.
+  } // End main
+  
+} // End class
+
